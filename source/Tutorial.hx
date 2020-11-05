626fa2a19a7abb5c8758a85c7e08db763cbbd583
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2020-11-05 11:57:15 -0800

    Created Tutorial state base

---
+++ source/Tutorial.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class Tutorial extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
