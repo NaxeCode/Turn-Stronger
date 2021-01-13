3c8b0015d16c66be2fe59bcb414168ec62f1b442
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-01-13 06:07:58 -0800

    Added lamp posts to tileset and tutorial level

--- assets/tiled/graveyard.tsx
+++ assets/tiled/graveyard.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="graveyard" tilewidth="16" tileheight="16" tilecount="50" columns="10">
- <image source="graveyard.png" width="160" height="80"/>
+<tileset version="1.4" tiledversion="1.4.2" name="graveyard" tilewidth="16" tileheight="16" tilecount="90" columns="10">
+ <image source="graveyard.png" width="160" height="144"/>
 </tileset>
