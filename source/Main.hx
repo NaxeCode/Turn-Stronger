97ac8986e0a1e5d1dac27060417716955d67b751
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-01-13 06:07:37 -0800

    Updated Game Resolution

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, Tutorial, 1, 60, 60, true));
+		addChild(new FlxGame(640, 480, Tutorial, 1, 60, 60, true));
 	}
 }
