911180521d95c3c5f25c412b84530980ee809a6e
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-01-13 04:14:32 -0800

    Implemented Type text support for the dialog box

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.addons.text.FlxTypeText;
 import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
@@ -9,25 +10,48 @@ import flixel.util.FlxColor;
 class DialogueBox extends FlxSpriteGroup
 {
 	var box:FlxSprite;
-	var text:FlxText;
+	var text:String;
+
+	var typeText:FlxTypeText;
 
 	public function new(X:Int = 0, Y:Int = 0, MXS:Int = 0)
 	{
 		super(X, Y, MXS);
 
+		initGraphicBox();
+		initTypeText();
+		this.visible = false;
+	}
+
+	function initGraphicBox()
+	{
 		box = new FlxSprite(0, 0);
 		box.makeGraphic(FlxG.width, Std.int(FlxG.height / 3), FlxColor.BLACK);
 		box.alpha = 0.5;
 		add(box);
 
 		this.y = (FlxG.height - this.height);
+	}
 
-		text = new FlxText(20, 15, FlxG.width / 2, "this is a really really really really really long sentence");
-		add(text);
+	function initTypeText()
+	{
+		typeText = new FlxTypeText(15, 10, this.box.frameWidth,
+			"Hello, this is a good example of some pretty long text that has to be typed down by any given character parsing dialog :)", 20);
+		add(typeText);
+	}
+
+	public function say(dialog:String)
+	{
+		typeText.resetText(dialog);
+		this.visible = true;
+		typeText.start(0.1, false, false, null);
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (this.visible == true) {}
+		else {}
 	}
 }
