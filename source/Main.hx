84b25d75514e761eb2cc8f853723612a91d44427
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-01-31 15:28:22 -0800

    Entry point is now BetterTutorial

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(640, 480, Physics, 1, 60, 60, true));
+		addChild(new FlxGame(640, 480, BetterTutorial, 1, 60, 60, true));
 	}
 }
