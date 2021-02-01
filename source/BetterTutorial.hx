12611dfde2cd90b983397146024b52c54b9d0492
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-01-31 17:33:38 -0800

    Added dialog box intigration with LDtk editor

--- source/BetterTutorial.hx
+++ source/BetterTutorial.hx
@@ -1,9 +1,17 @@
 package;
 
+import flixel.FlxCamera;
 import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.group.FlxSpriteGroup;
+import flixel.math.FlxMath;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
 
 class BetterTutorial extends FlxState
 {
@@ -12,9 +20,29 @@ class BetterTutorial extends FlxState
 	var collider:FlxSpriteGroup;
 	var background:FlxSpriteGroup;
 
+	public var noah:NPC;
+	public var npcs:FlxTypedGroup<NPC>;
+
+	public var dialogueBox:DialogueBox;
+
+	public static var gameCamera:FlxCamera;
+
+	var uiCamera:FlxCamera;
+
 	override public function create()
 	{
 		super.create();
+
+		FlxG.camera.flash(FlxColor.BLACK, 4);
+
+		dialogueBox = new DialogueBox();
+		add(dialogueBox);
+
+		gameCamera = new FlxCamera(0, 0, FlxG.width, FlxG.height);
+		uiCamera = new FlxCamera(0, 0, FlxG.width, FlxG.height);
+
+		gameCamera.bgColor = 0xFF17142d;
+		uiCamera.bgColor = FlxColor.TRANSPARENT;
 
 		var project = new LdtkProject();
 
@@ -49,12 +77,43 @@ class BetterTutorial extends FlxState
 			tile.immovable = true;
 		}
 		add(collider);
+
+		gameCamera.follow(player);
+		gameCamera.zoom = 2;
+
+		FlxG.cameras.reset(gameCamera);
+		FlxG.cameras.add(uiCamera);
+
+		FlxCamera.defaultCameras = [gameCamera];
+		// gameCamera.camera.setScrollBoundsRect(0, 0, background.width, background.height, true);
+		dialogueBox.cameras = [uiCamera];
+
+		dialogueBox.scrollFactor.set(0, 0);
 	}
 
 	function createEntities(entityLayer:LdtkProject.Layer_Entities)
 	{
-		var x = entityLayer.all_Player[0].pixelX;
-		var y = entityLayer.all_Player[0].pixelY;
+		var x:Int;
+		var y:Int;
+
+		x = entityLayer.all_Noah[0].pixelX;
+		y = entityLayer.all_Noah[0].pixelY;
+
+		npcs = new FlxTypedGroup<NPC>();
+
+		noah = new NPC(x, y);
+		noah.text = entityLayer.all_Noah[0].f_string;
+		noah.loadGraphic(AssetPaths.Noah__png, false, 32, 64);
+		noah.setFacingFlip(FlxObject.LEFT, true, false);
+		noah.setFacingFlip(FlxObject.RIGHT, false, false);
+
+		npcs.add(noah);
+		add(noah);
+
+		noah.facing = FlxObject.LEFT;
+
+		x = entityLayer.all_Player[0].pixelX;
+		y = entityLayer.all_Player[0].pixelY;
 
 		player = new Player(x, y);
 		add(player);
@@ -62,8 +121,41 @@ class BetterTutorial extends FlxState
 
 	override public function update(elapsed:Float)
 	{
+		handleDialogBox();
+
 		super.update(elapsed);
 
 		FlxG.collide(collider, player);
+	}
+
+	function handleDialogBox()
+	{
+		if (FlxG.keys.justPressed.Z)
+		{
+			if (dialogueBox.visible)
+			{
+				if (!dialogueBox.typeText._typing)
+				{
+					FlxTween.tween(gameCamera, {zoom: 2}, 0.5, {ease: FlxEase.backOut});
+					Reg.canMove = true;
+					dialogueBox.visible = false;
+				}
+				else
+				{
+					dialogueBox.typeText.skip();
+				}
+			}
+			else if (!dialogueBox.visible)
+			{
+				for (member in npcs)
+				{
+					if (FlxMath.isDistanceWithin(member, player, 75))
+					{
+						dialogueBox.say(member.text);
+						FlxTween.tween(gameCamera, {zoom: 4}, 0.5, {ease: FlxEase.backOut});
+					}
+				}
+			}
+		}
 	}
 }
