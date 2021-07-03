ba8854eadbf727553246811df8ec98d34914519e
Aladdin Lee <Lucas_Naruto_AE@Yahoo.com>
2021-07-03 11:00:31 -0700

    removed save functionality temporarily for android build

--- source/Main.hx
+++ source/Main.hx
@@ -1,29 +1,15 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxGame;
+import flixel.util.FlxSave;
 import openfl.display.Sprite;
 
 class Main extends Sprite
 {
 	public function new()
 	{
-		var startFullscreen:Bool = false;
-		var save:FlxSave = new FlxSave();
-		save.bind("TurnStronger");
-		#if desktop
-		if (save.data.fullscreen != null)
-		{
-			startFullscreen = save.data.fullscreen;
-		}
-		#end
-
 		super();
 		addChild(new FlxGame(640, 480, BetterTutorial, 1, 60, 60, true));
-
-		if (save.data.volume != null)
-		{
-			FlxG.sound.volume = save.data.volume;
-		}
-		save.close();
 	}
 }
