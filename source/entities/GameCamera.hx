cc23cb27c74d68cde854a42e581f17c469d91b45
NaxeCode <15963333+NaxeCode@users.noreply.github.com>
2023-08-29 13:15:27 -0700

    added in finite state machine for the flow of the dynamic camera operation

--- source/entities/GameCamera.hx
+++ source/entities/GameCamera.hx
@@ -3,12 +3,20 @@ package entities;
 import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.addons.util.FlxFSM;
 import flixel.math.FlxPoint;
+import flixel.util.FlxTimer;
 
 using tweenxcore.Tools;
 
 class GameCamera extends FlxCamera
 {
+	private var frameCount:Float = 0;
+
+	public static var TOTAL_FRAME:Int = 180; // 3 Second tween
+
+	public var fsm:FlxFSM<GameCamera>;
+
 	public var player:Player;
 
 	public var realTarget:FlxPoint;
@@ -16,9 +24,8 @@ class GameCamera extends FlxCamera
 	private var active_follow:Bool = false;
 	private var active_player:Bool = true;
 
-	private var frameCount:Float = 0;
-
-	public static var TOTAL_FRAME:Int = 180; // 3 Second tween
+	// Timer for playerStill
+	var playerStill:FlxTimer;
 
 	public function new(X:Int = 0, Y:Int = 0, Width:Int = 0, Height:Int = 0, Zoom:Float = 0)
 	{
@@ -27,11 +34,23 @@ class GameCamera extends FlxCamera
 		bgColor = 0xFF17142d;
 		antialiasing = false;
 
+		initFSM();
+
+		FlxG.watch.add(fsm, "state", "FSM state:");
+
 		FlxG.watch.add(this, "active_follow", "active_follow");
 		FlxG.watch.add(this, "active_player", "active_player");
 
 		FlxG.watch.add(scroll, "x", "scroll X");
 		FlxG.watch.add(scroll, "y", "scroll Y");
+	}
+
+	function initFSM()
+	{
+		fsm = new FlxFSM(this);
+		fsm.transitions.add(Idle, Follow, Conditions.follow);
+		fsm.transitions.add(Follow, Idle, Conditions.stopped);
+		fsm.transitions.start(Idle);
 	}
 
 	public function initPlayer(passThruPlayerObj:Player)
@@ -48,6 +67,7 @@ class GameCamera extends FlxCamera
 
 	override function update(elapsed:Float)
 	{
+		fsm.update(elapsed);
 		checkState();
 		flowState();
 
@@ -69,9 +89,6 @@ class GameCamera extends FlxCamera
 			initRealTarget();
 			updateRealTarget();
 		}
-
-		if (active_follow)
-			followPlayer();
 	}
 
 	function initRealTarget()
@@ -86,24 +103,6 @@ class GameCamera extends FlxCamera
 	function updateRealTarget()
 	{
 		realTarget.set(player.x - (this.width / 2.5), player.y - (this.height / 2.5));
-	}
-
-	function followPlayer()
-	{
-		var rate = frameCount / TOTAL_FRAME;
-
-		// An animation when rate is 0 to 1.
-		if (rate <= 1)
-		{
-			var rateX = rate.circInOut().lerp(scroll.x, realTarget.x);
-			var rateY = rate.circInOut().lerp(scroll.y, realTarget.y);
-			scroll.set(rateX, rateY);
-			frameCount += 0.25;
-		}
-		else
-		{
-			frameCount = 0;
-		}
 	}
 
 	function dynamicStateActive()
@@ -121,5 +120,67 @@ class GameCamera extends FlxCamera
 			scroll.y = rate.quintOut().lerp(player.y, player.y);
 		}
 		frameCount++;
+	}
+
+	override function destroy():Void
+	{
+		fsm.destroy();
+		fsm = null;
+		super.destroy();
+	}
+}
+
+class Conditions
+{
+	public static function follow(Owner:GameCamera):Bool
+	{
+		return (Owner.scroll.x != Owner.player.x - (Owner.width / 2.5) || Owner.scroll.y != Owner.player.y - (Owner.height / 2.5));
+	}
+
+	public static function stopped(Owner:GameCamera):Bool
+	{
+		return !(Owner.scroll.x != Owner.player.x - (Owner.width / 2.5) || Owner.scroll.y != Owner.player.y - (Owner.height / 2.5));
+	}
+}
+
+class Idle extends FlxFSMState<GameCamera>
+{
+	override function enter(owner:GameCamera, fsm:FlxFSM<GameCamera>):Void
+	{
+		// owner.animation.play("standing");
+	}
+
+	override function update(elapsed:Float, owner:GameCamera, fsm:FlxFSM<GameCamera>):Void {}
+}
+
+class Follow extends FlxFSMState<GameCamera>
+{
+	private var frameCount:Float = 0;
+
+	public static var TOTAL_FRAME:Int = 180; // 3 Second tween
+
+	override function enter(owner:GameCamera, fsm:FlxFSM<GameCamera>):Void {}
+
+	override function update(elapsed:Float, owner:GameCamera, fsm:FlxFSM<GameCamera>):Void
+	{
+		followPlayer(owner);
+	}
+
+	function followPlayer(owner:GameCamera)
+	{
+		var rate = frameCount / TOTAL_FRAME;
+
+		// An animation when rate is 0 to 1.
+		if (rate <= 1)
+		{
+			var rateX = rate.circInOut().lerp(owner.scroll.x, owner.realTarget.x);
+			var rateY = rate.circInOut().lerp(owner.scroll.y, owner.realTarget.y);
+			owner.scroll.set(rateX, rateY);
+			frameCount += 0.25;
+		}
+		else
+		{
+			frameCount = 0;
+		}
 	}
 }
