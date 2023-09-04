59f80828a43bfe6a19a258f96a87b28e86e3a4d2
NaxeCode <15963333+NaxeCode@users.noreply.github.com>
2023-09-04 05:28:42 -0700

    Updated to route to Playground.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,7 +1,7 @@
 package;
 
 import flixel.FlxGame;
-import levels.BetterTutorial;
+import levels.Playground;
 import openfl.display.Sprite;
 
 class Main extends Sprite
@@ -9,6 +9,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(640, 480, BetterTutorial, 60, 60, true));
+		addChild(new FlxGame(640, 480, Playground, 60, 60, true));
 	}
 }
